e81324f479c796bc9c79b1f9f9b6c9223123f4f7
Leonardo Ferreira <leoferstos5@gmail.com>
2026-08-12 19:19:41 -0300

    fix: root page web-admin

--- project/apps/web-admin/app/page.tsx
+++ project/apps/web-admin/app/page.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import Sidebar from "@/components/sidebar/Sidebar";
-import { useTheme } from "@/hooks/useTheme";
 
 export default function Home() {
-  useTheme('dark');
-
   return (
     <div>
-      <main className="">
+      <main>
         <Sidebar />
       </main>
     </div>
